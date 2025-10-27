a5d6e7ba3e2122a53bddb116731f0ff3825fe726
Txulki <49781082+Txulki@users.noreply.github.com>
2025-10-27 13:28:05 +0100

    Added load_devs_with_id
    Allows the including of an id at the end of each dev in the csv so we can determine the duplicates in the dataset and use it as a control dataset.

--- src/dev_fetcher.py
+++ src/dev_fetcher.py
@@ -27,3 +27,13 @@ def load_devs(file : str):
         for row in reader:
             DEVS.add((row[0], row[1]))
     return DEVS
+
+#I used this to include an id to load in a csv with the matches already determined to use as a control dataset.
+def load_devs_with_id(file : str):
+    DEVS = set()
+    with open(file, 'r', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        next(reader, None)
+        for row in reader:
+            DEVS.add((row[0], row[1], row[2]))
+    return DEVS
